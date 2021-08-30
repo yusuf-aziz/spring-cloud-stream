713689b2ad720a2bae5414d0738e2e6e901b4aac
Malkan <yusuf.aziz@gmail.com>
2021-08-30 18:19:08 +0530

    Add files via upload

---
+++ spring-cloud-stream-publisher/src/main/java/com/javaeg/cloud/stream/api/Product.java
@@ -0,0 +1,14 @@
+package com.javaeg.cloud.stream.api;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+
+    private int id;
+    private String name;
+}
---
+++ spring-cloud-stream-publisher/src/test/java/com/javaeg/cloud/stream/api/SpringCloudStreamPublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaeg.cloud.stream.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudStreamPublisherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
